69e089f29a892e214776e06f1e62f9ae67bcb5f2
Roberto  Albelda <robertoalbeldac@gmail.com>
2022-11-22 11:31:26 +0800

    added

---
+++ main.js
@@ -0,0 +1,12 @@
+const name = 'DX LABS'
+
+/** this code will return you an error */
+// name = 'DX TRANSFORMATION'
+
+/** and this one also */
+// const name = 'I LOVE THIS JOB'
+
+/** this will work  */
+// function  showName() {
+//   const name = 'DX TRANSFORMATION'
+// }
