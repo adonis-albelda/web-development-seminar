c06f84ad5d3b2f07db00bf31bf808cbba7500a55
Roberto  Albelda <robertoalbeldac@gmail.com>
2022-11-24 17:00:40 +0800

    addeed

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-const name = 'DX LABS'
+let showEvent = (a, b, ...others) => {
+  
+}
 
-/** this code will return you an error */
-// name = 'DX TRANSFORMATION'
-
-/** and this one also */
-// const name = 'I LOVE THIS JOB'
-
-/** this will work  */
-// function  showName() {
-//   const name = 'DX TRANSFORMATION'
-// }
+showEvent('DX', 'LABS', 'DX CAMP', '2022')
